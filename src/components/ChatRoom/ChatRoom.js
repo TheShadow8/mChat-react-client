109b619ce345a67c6567fef6a8bfedf5d1f083bd
TheShadow8 <minhdo.ca@gmail.com>
2018-12-12 13:47:33 -0500

    styled component, grid layouts

--- src/components/ChatRoom/ChatRoom.js
+++ src/components/ChatRoom/ChatRoom.js
@@ -1,14 +1,28 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export class ChatRoom extends Component {
-  render() {
-    return (
-      <div>
-        mChat
-        <p> Chat Romm </p>
-      </div>
-    );
-  }
-}
+import Channels from './SideBox/Channels';
+import Teams from './SideBox/Teams';
+import Header from './ChatBox/Header';
+import Messages from './ChatBox/Messages';
+import SendMessage from './ChatBox/SendMessage';
+import ChatRoomLayout from './ChatRoomLayout';
 
-export default ChatRoom;
+export default () => (
+  <ChatRoomLayout>
+    <Teams teams={[{ id: 1, letter: 'M' }, { id: 2, letter: 'D' }]} />
+    <Channels
+      teamName="Team name"
+      username="Username"
+      channels={[{ id: 1, name: 'general' }, { id: 2, name: 'random' }]}
+      users={[{ id: 1, name: 'slackbot' }, { id: 2, name: 'user1' }]}
+    />
+    <Header channelName="general" />
+    <Messages>
+      <ul className="message-list">
+        <li />
+        <li />
+      </ul>
+    </Messages>
+    <SendMessage channelName="general" />
+  </ChatRoomLayout>
+);
